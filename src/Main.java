1ae30a0022fcc2f26a50f96bb7ada83e270584c9
Sergio García <garciparedes@gmail.com>
2014-10-08 11:40:09 +0200

    Creación de clase main

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author segarci & albamig
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
