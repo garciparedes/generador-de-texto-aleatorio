4c758b86ffaeea466aee1638a561e11193a1ed57
Sergio García <garciparedes@gmail.com>
2014-10-11 20:13:21 +0200

    Creada clase Text y implementada entrada del user
    El  texto se introduce desde la clase main, pero es la clase Text la
    que abre el fichero y lee los datos que hay en el

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,72 @@
+
 /**
  * 
  */
+
+import java.util.Scanner;
 
 /**
  * @author segarci & albamig
  */
 public class Main {
 
-	/**
+    private static final String WELCOME = "Bienvenido al generador aleatorio de textos";
+    private static final String INTRODUCE_LINEA = "Introduzca el numero de caracteres que tendra el texto que desea generar:";
+    private static final String INTRODUCE_TEXTO = "Introduzca el nombre del fichero que contiene el texto base:";
+    private static final String ERROR_LECTURA_ENTERO = "Error al introducir el numero";
+
+    private static Scanner sc;
+    private static String fileName;
+    private static int textLength;
+
+    //Variable para hacer llamadas a clase Text
+    private static Text text;
+
+    private static String finalText;
+
+    /**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola");
-		System.out.println();
+
+        text = new Text();
+
+        System.out.println(WELCOME);
+
+        //Leemos el numero de lineas que tendra el fichero
+        textLength = writeInt(INTRODUCE_LINEA, ERROR_LECTURA_ENTERO);
+
+        //Leemos el nombre de fichero
+        fileName = writeString(INTRODUCE_TEXTO);
+
+        //Leemos el contenido del fichero
+        finalText = text.readFile(fileName);
+
+        System.out.println(finalText);
+
 	}
+
+    public static int writeInt(String message, String errorMessage){
+        System.out.println(message);
+
+        try{
+            sc = new Scanner(System.in);
+            textLength = sc.nextInt();
+
+        }catch (Exception InputMismatchException){
+            System.out.println(errorMessage);
+            textLength = writeInt(message, errorMessage);
+
+        }
+
+        return textLength;
+    }
+
+    public static String writeString(String message){
+        System.out.println(message);
+        sc = new Scanner(System.in);
+
+        return sc.nextLine();
+    }
 
 }
---
+++ src/Text.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ *
+ * Created by garciparedes on 11/10/14.
+ */
+public class Text {
+
+    private final String ERROR_FICHERO = "Error: Fichero no encontrado";
+    private final String ROUTE = "texts/";
+
+    private File file;
+    private String text;
+    private FileReader fr;
+    private BufferedReader br;
+
+    //Variable para hacer llamadas a clase Main
+    private Main main = new Main();
+
+    public String readFile(String fileName){
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            file = new File(ROUTE + fileName);
+
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            String textBuffer = "";
+            while((linea=br.readLine())!=null) {
+                textBuffer = textBuffer + linea;
+            }
+            text = textBuffer;
+
+        }catch(Exception FileNotFoundException){
+            System.out.println(ERROR_FICHERO);
+            fileName = main.writeString();
+            text = readFile(fileName);
+
+
+        }finally{
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try{
+                if( null != fr ){
+                    fr.close();
+                }
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+
+        return text;
+    }
+
+
+}
