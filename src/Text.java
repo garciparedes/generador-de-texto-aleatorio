8f7c7a7fb4a9a177bca2305ba9e3c23f7941ba47
Sergio García <garciparedes@gmail.com>
2014-11-13 19:58:19 +0100

    Cambios hechos en clase

--- src/Text.java
+++ src/Text.java
@@ -9,29 +9,32 @@ import java.util.*;
  */
 public class Text {
 
-	private StringBuilder text;
+    private StringBuilder text;
+    private int lenghtText;
     public static StringBuilder oriText;
 
     //Constructor que crea el objeto texto a partir de otro anterior
-    public Text(String fileName, int refi, int lenght){
+    public Text(String fileName, int refi, int lenghtText) {
         oriText = readFile(fileName);
-        this.text = genText( refi, lenght);
+        this.text = genText(refi);
+        this.lenghtText = lenghtText;
+
     }
 
-	public StringBuilder getText(){
-		return text;
-	}
+    public StringBuilder getText() {
+        return text;
+    }
 
-	//Lee desde fichero y genera el texto con el contenido que hay en ��l
-	public static StringBuilder readFile(String fileName){
-		final String ERROR_FICHERO = "Error: Fichero no encontrado";
-	    final String ROUTE = "texts/";
-	    
-	    File file;
-	    FileReader fr = null;
-	    BufferedReader br;
-        
-	    StringBuilder textBuilder = new StringBuilder();
+    //Lee desde fichero y genera el texto con el contenido que hay en ��l
+    public static StringBuilder readFile(String fileName) {
+        final String ERROR_FICHERO = "Error: Fichero no encontrado";
+        final String ROUTE = "texts/";
+
+        File file;
+        FileReader fr = null;
+        BufferedReader br;
+
+        StringBuilder textBuilder = new StringBuilder();
 
         try {
             // Apertura del fichero y creacion de BufferedReader para poder
@@ -43,108 +46,39 @@ public class Text {
 
             // Lectura del fichero
             String linea;
-            while((linea=br.readLine())!=null) {
+            while ((linea = br.readLine()) != null) {
                 textBuilder.append(linea).append('\n');
             }
 
-        }catch(Exception FileNotFoundException){
+        } catch (Exception FileNotFoundException) {
             System.out.println(ERROR_FICHERO);
             fileName = Main.writeString(Main.INTRODUCE_TEXTO);
             Text.readFile(fileName);
 
-        }finally{
+        } finally {
             // En el finally cerramos el fichero, para asegurarnos
             // que se cierra tanto si todo va bien como si salta
             // una excepcion.
-            try{
-                if( null != fr ){
+            try {
+                if (null != fr) {
                     fr.close();
                 }
-            }catch (Exception e2){
+            } catch (Exception e2) {
                 e2.printStackTrace();
             }
         }
 
 
         // Para evitar problemas de límite
-        textBuilder.deleteCharAt(textBuilder.length()-1);
+        textBuilder.deleteCharAt(textBuilder.length() - 1);
 
         return textBuilder;
     }
 
 
-	//genera un texto aleatorio a partir de los parametros que se le manda
-    private StringBuilder genText(int refi, int lenght){
+    //genera un texto aleatorio a partir de los parametros que se le manda
+    private StringBuilder genText(int refi) {
+        WordList[] multiMatriz = WordList.newInstance(refi);
 
-        StringBuilder strText = new StringBuilder();
-
-        Date viejo = new Date();
-        WordList[] multiMatriz = WordList.newInstance( refi);
-        Date nuevo = new Date();
-
-        System.out.println((double)(nuevo.getTime() - viejo.getTime())/1000);
-
-        System.out.println(multiMatriz.length);
-        for(int i = 0; i < multiMatriz.length; i++){
-            System.out.println(multiMatriz[i].getLetter() + " " + multiMatriz[i].getPositionList().length);
-        }
-
-        System.out.println(multiMatriz[0].getWordLists().length);
-        for(int i = 0; i < multiMatriz[0].getWordLists().length; i++){
-            System.out.println(multiMatriz[0].getWordLists()[i].getLetter() + " "
-                    + multiMatriz[0].getWordLists()[i].getPositionList().length
-                    );
-        }
-
-        int rand;
-
-        if (refi == 0){
-            while (lenght > 0 ){
-                rand = (int)(Math.random() * multiMatriz.length);
-
-                strText.append(multiMatriz[rand].getLetter());
-                lenght--;
-            }
-        } else {
-
-            while (lenght > 0 ){
-                lenght = putChar(lenght, multiMatriz);
-            }
-
-        }
-
-        return strText;
     }
-
-    private int putChar(int lenght, WordList[] multiMatriz){
-
-        try {
-            int numLetters = WordList.getArrayLenght(multiMatriz);
-
-            if (lenght > 0 && numLetters != 0) {
-
-                int i = 0, valor = 0;
-
-                int rand = (int) (Math.random() * numLetters);
-
-                //Acumulador del numero de letras
-                while (rand > valor) {
-                    try { valor += multiMatriz[i].getPositionList().length; }
-                    catch (NullPointerException e){}
-                    i++;
-                }
-
-                try {
-
-                    System.out.print(multiMatriz[i - 1].getLetter());
-                    lenght = putChar(lenght - 1, multiMatriz[i - 1].getWordLists());
-
-                } catch (IndexOutOfBoundsException ignore){}
-            }
-
-        } catch (NullPointerException e){}
-
-        return lenght;
-    }
-
 }
--- src/WordList.java
+++ src/WordList.java
@@ -1,197 +1,67 @@
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 public class WordList {
     private char letter;
-    private Integer[] positionList;
-    private WordList[] wordLists;
+    private int numLetter;
+    private WordList[] continueLetter;
 
-    private static WordList[] defaultWordList = new WordList[0];
-
-    public static WordList[] getDefaultWordList() {
-
-        WordList[] newWordList = new WordList[defaultWordList.length];
-        for(int i = 0 ; i < defaultWordList.length ; i++){
-            newWordList[i] = new WordList(defaultWordList[i].getLetter(), null, null);
-        }
-        return newWordList;
-    }
-
-    public static WordList[] newLetter(WordList[] oldArray, WordList wordList){
-        WordList[] newArray = new WordList[oldArray.length+1];
-        for (int i =0 ; i< oldArray.length; i++){
-            newArray[i] = new WordList(oldArray[i].getLetter(), oldArray[i].getPositionList(), null);
-        }
-        newArray[newArray.length-1] = wordList;
-
-        return newArray;
-    }
-
-    public static Integer[] newPosition(Integer[] oldArray, int newNum){
-        Integer[] newArray = new Integer[oldArray.length+1];
-        for (int i =0 ; i< oldArray.length; i++){
-            newArray[i] = oldArray[i];
-        }
-        newArray[newArray.length-1] = newNum;
-
-        return newArray;
-    }
-
-    public WordList(char letter, Integer[] positionList, WordList[] wordLists) {
+    public WordList(char letter, int numLetter, WordList[] continueLetter){
         this.letter = letter;
-        this.positionList = positionList;
-        this.wordLists = wordLists;
+        this.numLetter = numLetter;
+        this.continueLetter = continueLetter;
     }
-
-
-    public void setPositionList(Integer[] positionList) {
-        this.positionList = positionList;
-    }
-
-
-    public void setWordLists(WordList[] wordLists) {
-        this.wordLists = wordLists;
-    }
-
-
-    public char getLetter() {
-        return letter;
-    }
-
-
-    public Integer[] getPositionList() {
-        return positionList;
-    }
-
-
-    public WordList[] getWordLists() {
-        return wordLists;
-    }
-
-
-    private static int containsLetter(WordList[] array, char letter){
-
-        for (int i = 0 ; i< array.length ; i++){
-            if (array[i].getLetter() == letter){
-                return i;
-            }
-        }
-        return -1;
-    }
-
 
     public static WordList[] newInstance(int refi){
-        WordList[] newWordList = new WordList[0];
-        Integer[] newPositionList = new Integer[0];
-
+        WordList[] multimatriz = new WordList[0];
+        Character[] letterArray = new Character[0];
         char charLetter;
 
-        int iterator ;
-
-        //Genera el primer nivel del arbol
-        for (int position = 0; position < Text.oriText.length(); position++) {
-            charLetter = Text.oriText.charAt(position);
-
-            iterator = containsLetter(newWordList, charLetter);
-            if ( iterator != -1) {
-
-                //newWordList[iterator].getPositionList().add(position);
-
-                newWordList[iterator].setPositionList(newPosition(newWordList[iterator].getPositionList(), position));
-
-            } else {
-
-
-                newPositionList = newPosition(newPositionList, position);
-
-                newWordList = newLetter(newWordList, new WordList(charLetter, newPositionList, null));
-
-                defaultWordList = newLetter(defaultWordList, new WordList(charLetter, newPositionList, null));
-
-                newPositionList = new Integer[0];
-
-            }
-        }
-
-        if (refi > 1) {
-
-            for (int i = 0; i < newWordList.length; i++){
-               newWordList[i].addLevel(refi-1);
+        for(int i = 0 ; i < Text.oriText.length() ; i++ ){
+            charLetter = Text.oriText.charAt(i);
+            if (!containsArray(letterArray, charLetter)){
+                nuevaLetra(letterArray, charLetter);
             }
 
-        }
 
-        return newWordList;
+        }
+        multimatriz =
+
+
+        return multimatriz;
     }
 
-
-    public void addLevel(int refi) {
-        WordList[] newWordList = getDefaultWordList();
-
-        //for(int i = 0; i< newWordList.length)
-
-        Integer[] newPositionList;
-
-        Integer[] positionList = this.getPositionList();
-
-        char charLetter;
-
-        try {
-
-
-            for (int i = 0; i < positionList.length; i++) {
-                try {
-
-
-                    charLetter = Text.oriText.charAt(positionList[i]+1);
-
-                    newPositionList = newWordList[containsLetter(newWordList, charLetter)].getPositionList();
-
-
-
-                    if (newPositionList == null) {
-
-                        newPositionList = new Integer[0];
-                    }
-
-                    //newPositionList.add(positionList[i]+1);
-
-                    newPositionList = newPosition(newPositionList, positionList[i]+1);
-
-                    //newWordList[containsLetter(newWordList, charLetter)].setPositionList(newPositionList);
-
-
-                } catch (StringIndexOutOfBoundsException ignored){}
-
-
-            }
-
-        } catch (NullPointerException ignored){}
-
-        this.setWordLists(newWordList);
-
-        if (refi > 1){
-
-            for (int i = 0; i < newWordList.length; i++) {
-                newWordList[i].addLevel(refi - 1);
+    public static boolean containsArray(Character[] letterArray, char charLetter){
+        for (int i = 0 ; i < letterArray.length ; i++){
+            if (letterArray[i] == charLetter){
+                return true;
             }
         }
+
+        return false;
     }
 
+    public static Character[] nuevaLetra(Character[] oldLetterArray, char charLetter){
+        Character[] newLetterArray = new Character[oldLetterArray.length + 1];
 
-
-    public static int getArrayLenght (WordList[] array){
-        int size = 0;
-
-        for (WordList positions: array){
-
-            try {
-
-                size += positions.getPositionList().length;
-
-            }catch (NullPointerException e){}
+        for (int i =0 ; i < oldLetterArray.length ; i++ ){
+            newLetterArray[i] = oldLetterArray[i];
         }
+        newLetterArray[newLetterArray.length-1] = charLetter;
+    }
 
-        return size;
+    public static WordList[] creaMultiMatriz(Character[] letterArray, int dimension){
+
+        WordList[] multimatriz = new WordList[letterArray.length];
+
+        if (dimension != 0) {
+
+            for (int i = 0; i < letterArray.length; i++) {
+                multimatriz[i] = new WordList(letterArray[i], 0, creaMultiMatriz(letterArray, dimension - 1));
+
+            }
+            return multimatriz;
+
+        } else { return null; }
     }
 }
