f52bb2142079b7e0407f651a2a304729dd8528f0
Sergio García <garciparedes@gmail.com>
2014-12-21 14:05:15 +0100

    Modelada clase MyList
    La he empezado a hacer como si fuese un ArrayList, pero una vez que
    esté convertido el código de la practica a esta implementación
    probaremos haber si es más rápido usando LinkedList y en el caso de que
    así sea se convertirá el código a este nuevo caso

--- src/MyList.java
+++ src/MyList.java
@@ -1,9 +1,59 @@
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * Created by garciparedes on 4/12/14.
  */
 public class MyList<E> {
+
+    
+    private Object[] array;
+
+
+    /**
+     *
+     */
+    public MyList (){
+        this.array = new Object[0];
+    }
+
+
+    /**
+     *
+     * @param defSize
+     */
+    public MyList (int defSize){
+        this.array = new Object[defSize];
+    }
+
+
+    /**
+     *
+     * @param element
+     */
+    public void add(E element){
+        array = Arrays.copyOf(array, array.length+1);
+        array[array.length-1] = element;
+
+    }
+
+
+    /**
+     *
+     * @param i
+     * @return
+     */
+    public E get(int i){
+        return (E) array[i];
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public int size(){
+        return array.length;
+    }
     
 }
 
